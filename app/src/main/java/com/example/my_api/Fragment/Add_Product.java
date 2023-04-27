5c40b5e376778d220dd0be6167833e5bd6924bdd
c18 <12>
2023-04-27 11:09:40 +0530

    6

--- app/src/main/java/com/example/my_api/Fragment/Add_Product.java
+++ app/src/main/java/com/example/my_api/Fragment/Add_Product.java
@@ -1,7 +1,13 @@
 package com.example.my_api.Fragment;
 
+import static android.app.Activity.RESULT_OK;
+
+import android.Manifest;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -12,15 +18,23 @@ import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.example.my_api.Activity.SplashScreen;
+import com.example.my_api.Models.ProductData;
 import com.example.my_api.R;
 import com.example.my_api.Retrofit.Retro_Instance_Class;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 
 public class Add_Product extends Fragment {
-EditText proname,proprice,prodes;
-ImageView proimage;
-Button addpro;
-String userid;
+    EditText et1,et2,et3;
+    ImageView imageView;
+    Button addpro;
+    String userid;
+    String proname,proprice,prodes,proimage;
+
+    String per[]={Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.INTERNET};
 
 
     @Override
@@ -28,21 +42,53 @@ String userid;
                              Bundle savedInstanceState) {
 
          View view=inflater.inflate(R.layout.fragment_add_product, container, false);
-         proname=view.findViewById(R.id.pro_name);
-         proprice=view.findViewById(R.id.pro_price);
-         prodes=view.findViewById(R.id.pro_des);
-         proimage=view.findViewById(R.id.pro_Image);
+         et1=view.findViewById(R.id.pro_name);
+         et2=view.findViewById(R.id.pro_price);
+         et3=view.findViewById(R.id.pro_des);
+         imageView=view.findViewById(R.id.pro_Image);
          addpro=view.findViewById(R.id.btnadd_pro);
+
+         imageView.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                 CropImage.activity()
+                         .setGuidelines(CropImageView.Guidelines.ON)
+                         .start(Add_Product.this);
+             }
+         });
 
          userid= SplashScreen.preferences.getString("userid","");
 
          addpro.setOnClickListener(new View.OnClickListener() {
              @Override
              public void onClick(View view) {
-
+                addproduct();
              }
          });
 
+
         return view;
+    }
+
+
+
+
+    private void addproduct()
+    {
+        proname=et1.getText().toString();
+        proprice=et2.getText().toString();
+        prodes=et3.getText().toString();
+
+//        Retro_Instance_Class.MyAPICalling().addproduct(proname,proprice,prodes).enqueue(new Callback<ProductData>() {
+//            @Override
+//            public void onResponse(Call<ProductData> call, Response<ProductData> response) {
+//
+//            }
+//
+//            @Override
+//            public void onFailure(Call<ProductData> call, Throwable t) {
+//
+//            }
+//        });
     }
 }
